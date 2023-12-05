52c4f175849c3ba47c9c9754d31146f76ef8990f
DHRUVIK-1811 <dhruvik1811@gmail.com>
2023-12-06 01:08:08 +0530

    first commit

---
+++ constants/constants.js
@@ -0,0 +1,58 @@
+export const NAME = 'Dhruvik Vaghasiya';
+
+export const DESIGNATION = `MERN Stack Developer | NodeJs Developer | ReactJs Develpoer`;
+
+export const DETAILS = {
+  Residence: 'India',
+  City: 'Surat',
+  Age: '20',
+};
+
+export const SKILLS = [
+  {
+    title: 'MERN Stack Developer',
+    level: '70%',
+  },
+  {
+    title: 'Node Developer',
+    level: '88%',
+  },
+  {
+    title: 'React Developer',
+    level: '75%',
+  },
+  {
+    title: 'Next Developer',
+    level: '75%',
+  },
+];
+
+export const TECH_STACK = [
+  'ReactJS',
+  'JavaScript',
+  'NextJS',
+  'TypeScript',
+  'NodeJS',
+  'Redux',
+  'CSS',
+  'SCSS',
+  'TailwindCSS',
+  'Material UI',
+  'Next UI',
+  'REST API',
+  'MongoDB',
+  'MySQL',
+  'Git',
+  'GitHub',
+];
+
+export const CONTACTS = {
+  EMAIL: 'dhruvikvaghasiya1811@gmail.com',
+  PHONE: '+91 6355968944',
+};
+export const SOCIAL_LINKS = {
+  GITHUB: 'https://github.com/DHRUVIK-1811',
+  LINKEDIN: 'https://in.linkedin.com/in/dhruvik-vaghasiya-a90344258',
+  INSTAGRAM: 'https://instagram.com/ll._dhruvik_vaghasiya_.ll?igshid=OGQ5ZDc2ODk2ZA==',
+  FACEBOOK: 'https://www.facebook.com/dhruvik.vaghasiya.5?mibextid=JRoKGi',
+};
